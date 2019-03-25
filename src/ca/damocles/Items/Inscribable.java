3a5962dcba056173e67c88d61ab75e24cd5a1638
Nicholas Ryan <nnryanp@gmail.com>
2019-03-25 13:16:55 -0700

    Start adding enchantments (runes)

---
+++ src/ca/damocles/Items/Inscribable.java
@@ -0,0 +1,7 @@
+package ca.damocles.Items;
+
+public interface Inscribable {
+	
+	public List<Rune> getAppliedRunes();
+	
+}
---
+++ src/ca/damocles/Runes/Rune.java
@@ -0,0 +1,15 @@
+package ca.damocles.Runes;
+
+public class Rune {
+	
+	int id;
+	
+	public Rune(int id) {
+		this.id = id;
+	}
+	
+	public int getID() {
+		return id;
+	}
+	
+}
