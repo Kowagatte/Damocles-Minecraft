e9b0a0f0fd75f5a6ca9660d84c18f375622a3ac4
Nicholas Ryan <nnryanp@gmail.com>
2019-03-27 11:32:33 -0700

    Finished Sword Factory

--- src/ca/damocles/Events/PlayerShiftClick.java
+++ src/ca/damocles/Events/PlayerShiftClick.java
@@ -1,13 +1,11 @@
 package ca.damocles.Events;
 
-import java.util.Arrays;
-
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-import ca.damocles.Menus.Book.Book;
+import ca.damocles.Items.Factories.SwordFactory;
 
 public class PlayerShiftClick implements Listener{
 	
@@ -15,9 +13,12 @@ public class PlayerShiftClick implements Listener{
 	public void onPlayerClick(PlayerInteractEvent event) {
 		if(event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 			if(event.getPlayer().isSneaking()) {
-				Book book = new Book("Info", "GOD");
-				book.addPage(Arrays.asList("Test"));
-				book.openBook(event.getPlayer());
+				event.getPlayer().getInventory().addItem(
+						new SwordFactory(1)
+						.setUpDurability(100)
+						.setAttackSpeed(-2.4)
+						.setDamage(7)
+						.build());
 			}
 		}
 	}
--- src/ca/damocles/Items/Factories/SwordFactory.java
+++ src/ca/damocles/Items/Factories/SwordFactory.java
@@ -1,40 +1,50 @@
 package ca.damocles.Items.Factories;
 
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import ca.damocles.Items.Types.Sword;
+import ca.damocles.Runes.Rune;
+
 public class SwordFactory {
+	
+	Sword sword;
 	
 	public SwordFactory(int customModelID) {
 		ItemStack item = new ItemStack(Material.DIAMOND_SWORD, 1);
 		ItemMeta meta = item.getItemMeta();
-		Damageable dmeta = (Damageable)meta;
-		dmeta.setDamage(customModelID);
-		item.setItemMeta((ItemMeta)dmeta);
-		meta = item.getItemMeta();
-		
+		((Damageable)meta).setDamage(customModelID);
+		meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_UNBREAKABLE, ItemFlag.HIDE_ENCHANTS);
+		meta.setUnbreakable(true);
+		item.setItemMeta(meta);
+		sword = new Sword(item);
 	}
 	
 	public SwordFactory setUpDurability(int maxdurability) {
-		
+		sword.setDurability(maxdurability);
+		return this;
 	}
 	
 	public SwordFactory setDamage(int damage) {
-		
+		sword.setDamage(damage);
+		return this;
 	}
 	
 	public SwordFactory setAttackSpeed(double attackSpeed) {
-		
+		sword.setAttackSpeed(attackSpeed);
+		return this;
 	}
 	
 	public SwordFactory addRune(Rune rune, int level) {
-		
+		sword.addRune(rune, level);
+		return this;
 	}
 	
 	public ItemStack build() {
-		
+		return sword.finish();
 	}
 	
 }
